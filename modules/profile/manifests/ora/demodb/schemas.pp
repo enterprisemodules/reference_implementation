9b98ade84ef996518133ae48fa8708d3cc348ff9
Bert Hajee <bert.hajee@enterprisemodules.com>
2016-06-14 14:56:44 +0200

    raname database and TS
    commit fbb06b27544ef1137097f7d048cce22a56dd6450
    Author: Bert Hajee <bert.hajee@enterprisemodules.com>
    Date:   Wed Jun 1 21:09:47 2016 +0200
    
        Add better way of patching oracle database

--- modules/profile/manifests/ora/demodb/schemas.pp
+++ modules/profile/manifests/ora/demodb/schemas.pp
@@ -1,13 +1,13 @@
 class profile::ora::demodb::schemas(
   String  $schema_name,
-  String $dbname,
+  String  $dbname,
 ){
   ora_user{"${schema_name}@${dbname}":
-    ensure     => present,
-    password   => $schema_name,
-    quotas     => {
-      'SYSTEM' => 0,
-      'DEMO'   => 'unlimited',
+    ensure      => present,
+    password    => $schema_name,
+    quotas      => {
+      'SYSTEM'  => 0,
+      'DEMO_TS' => 'unlimited',
     },
     grants    => [
       'CONNECT'
